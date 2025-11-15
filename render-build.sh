28359d437b6533cb61a3ff76e415e4162275126a
Ribhav-Singla <ribhavsingla2166@gmail.com>
2025-11-15 18:16:30 +0530

    Fix BullMQ module resolution - downgrade to v4 and clean install

--- render-build.sh
+++ render-build.sh
@@ -4,7 +4,8 @@ set -e
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Install Node dependencies
+# Clean install Node dependencies
+rm -rf node_modules package-lock.json
 npm install
 
 # Install PM2 globally
